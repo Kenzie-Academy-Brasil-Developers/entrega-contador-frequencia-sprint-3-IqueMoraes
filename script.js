9223c3f24be85fce514d452a585b70380daa21c6
Ique Moraes <iquedemoraes@hotmail.com>
2021-07-19 20:43:43 -0300

    realizados os contadores de letras e palavras

---
+++ script.js
@@ -0,0 +1,80 @@
+const textBox = document.querySelector('#textInput');
+
+function selecionarLetras(x) { 
+
+       let textoEscrito = x.value;
+       textoEscrito = textoEscrito.toLowerCase();
+       textoEscrito = textoEscrito.replace(/[^a-z'\s]+/g, ""); 
+       let textoEscritoArray = textoEscrito.split(""); 
+        return textoEscritoArray;}
+
+function selecionarPalavras(x) { 
+
+    let textoEscrito = x.value;
+    textoEscrito = textoEscrito.toLowerCase();
+    textoEscrito = textoEscrito.replace(/[^a-z'\s]+/g, ""); 
+    let textoEscritoArray = textoEscrito.split(" "); 
+        return textoEscritoArray;}
+
+
+
+const contarLetras = (x) => {
+    let letterCounts = {};
+
+    for (let i = 0; i < x.length; i++) {
+        currentLetter = x[i];
+        // if (letterCounts[currentLetter] === ' ') { como retirar os espaços?
+        // }
+        if (letterCounts[currentLetter] === undefined) {
+            letterCounts[currentLetter] = 1; 
+        } else { 
+            letterCounts[currentLetter]++; 
+        }
+    }
+
+    for (let letter in letterCounts) { 
+        const span = document.createElement("span"); 
+        const textContent = `"${letter}": ${letterCounts[letter]}, `;
+        span.innerText = textContent; 
+        const letters = document.getElementById("lettersDiv");
+        letters.appendChild(span); 
+     }
+}
+
+const contarPalavras = (x) => {
+    let wordsCounts = {};
+
+    for (let i = 0; i < x.length; i++) {
+        currentWord = x[i];
+        if (wordsCounts[currentWord] === undefined) {
+            wordsCounts[currentWord] = 1; 
+        } else { 
+            wordsCounts[currentWord]++; 
+        }
+    }
+
+    for (let word in wordsCounts) { 
+        const span = document.createElement("span"); 
+        const textContent = `"${word}": ${wordsCounts[word]}, `;
+        span.innerText = textContent; 
+        const words = document.getElementById("wordsDiv");
+        words.appendChild(span); 
+     }
+}
+
+const button = document.getElementById("countButton");
+button.addEventListener("click", function() {  
+   // teu código vai aqui ... 
+   const typedText = selecionarLetras(textBox);
+   contarLetras(typedText);
+
+   const typedWords = selecionarPalavras(textBox);
+   contarPalavras(typedWords);
+
+});
+
+
+
+
+
+
